f5b84c624ec72a3691785a47d16c3dcb541f5ea4
gslopez <gslopez@uc.cl>
2013-01-17 20:43:28 -0300

    Indice de setData, en Modulo cambiado

--- src/com/example/controlador/Modulo.java
+++ src/com/example/controlador/Modulo.java
@@ -434,7 +434,7 @@ public class Modulo extends Modelo {
 	@Override
 	public void setData(String id, Object[] params) {
 
-		for (int i = 1; i < keys.length; i++) {
+		for (int i = 0; i < keys.length; i++) {
 			this.params.put(this.keys[i], params[i]);
 		}
 
