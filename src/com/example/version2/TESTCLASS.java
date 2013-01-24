55243160e00e3ef2a8771d900da985dc8188040d
gslopez <gslopez@uc.cl>
2013-01-24 00:56:12 -0300

    archivo de convencion JSON de servidor agregado, y una clase que vale cayampa

---
+++ src/com/example/version2/TESTCLASS.java
@@ -0,0 +1,19 @@
+package com.example.version2;
+
+import android.view.KeyEvent;
+import android.view.View;
+
+public class TESTCLASS implements View.OnClickListener, View.OnKeyListener{
+
+	public boolean onKey(View v, int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
